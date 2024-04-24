192d46bd9cbf313bd5d1588cc41d7251f2c3547e
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-04-24 17:59:13 +0500

    profile Screen

--- all_fixs/lib/Views/Profile/profile.dart
+++ all_fixs/lib/Views/Profile/profile.dart
@@ -1,9 +1,26 @@
 import "package:flutter/material.dart";
+import "package:google_fonts/google_fonts.dart";
+
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        SizedBox(height: 10,),
+        Text(
+          "My Profile",
+          style: GoogleFonts.bodoniModa(
+              fontSize: 30,
+              color: Colors.orange[900],
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1),
+        ),
+        Container(
+          
+        )
+      ],
+    );
   }
 }
