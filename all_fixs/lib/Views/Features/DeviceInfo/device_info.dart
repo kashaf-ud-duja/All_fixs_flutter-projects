b4061f98d00463a2748de3e3cf6328a41862fa35
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-04-30 23:59:33 +0500

    implemenetin changes

--- all_fixs/lib/Views/Features/DeviceInfo/device_info.dart
+++ all_fixs/lib/Views/Features/DeviceInfo/device_info.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
 class DeviceInfo extends StatelessWidget {
   const DeviceInfo({super.key});
 
@@ -13,6 +14,25 @@ class DeviceInfo extends StatelessWidget {
         ),
         backgroundColor: Color.fromRGBO(230, 81, 0, 1),
         centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.black,
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xFFFFB74D), Color(0xFFFB8C00)]),
+            ),
+          )
+        ],
       ),
     );
   }
