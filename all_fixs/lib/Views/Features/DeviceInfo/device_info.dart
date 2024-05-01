447da80743c133e336e03dd39c47b1a98f783ec0
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-05-01 23:38:54 +0500

    device info module UI

--- all_fixs/lib/Views/Features/DeviceInfo/device_info.dart
+++ all_fixs/lib/Views/Features/DeviceInfo/device_info.dart
@@ -25,13 +25,21 @@ class DeviceInfo extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            height: 50,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  colors: [Color(0xFFFFB74D), Color(0xFFFB8C00)]),
-            ),
-          )
+              height: 50,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFFFFB74D), Color(0xFFFB8C00)],
+                ),
+              ),
+              child: Text(
+                "Device Info",
+                style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 12,
+                    color: Colors.black,
+                    letterSpacing: 1),
+              ))
         ],
       ),
     );
