4625fa1c3f9937b81fd99173401d8edaf5b32ecd
kashaf-ud-duja <tahirkashaf7@gmail.com>
2024-05-21 11:46:18 +0500

    dashboard-userstatus

--- all_fixs/lib/Views/Features/DeviceInfo/UserStatus/user_status.dart
+++ all_fixs/lib/Views/Features/DeviceInfo/UserStatus/user_status.dart
@@ -1,12 +1,10 @@
+import 'package:all_fixs/Views/Widgets/Features/DeviceInfo/two_value_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TwoValueCard({required String text, required String value}) {
-  
-}
 class UserStatus extends StatelessWidget {
   const UserStatus({super.key});
 
@@ -18,7 +16,7 @@ class UserStatus extends StatelessWidget {
           "ALL FIXS",
           style: GoogleFonts.poppins(fontWeight: FontWeight.bold),
         ),
-        backgroundColor: Color.fromRGBO(230, 81, 0, 1),
+        backgroundColor: Colors.transparent,
         centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_ios),
@@ -29,6 +27,7 @@ class UserStatus extends StatelessWidget {
         ),
       ),
       body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [
@@ -62,11 +61,11 @@ class UserStatus extends StatelessWidget {
                       ),
                     ),
                     SizedBox(
-                      height: 50,
+                      height: 20,
                     ),
                     Expanded(
                       child: Container(
-                        color: Colors.pink,
+                        color: Colors.transparent,
                         child: Row(
                           children: [
                             Expanded(
@@ -74,13 +73,26 @@ class UserStatus extends StatelessWidget {
                                 children: [
                                   Expanded(
                                     child: Container(
-                                        color: Colors.red,
-                                        child:TwoValueCard(text: "User is", value: "Online")
-                                         ),
+                                      color:Colors.transparent,
+                                      child: TwoValueCard(
+                                        text: "User is",
+                                        txtclr: Colors.orange[900],
+                                        value: "Online",
+                                        subwidget: null,
+                                        clr: null,
+                                      ),
+                                    ),
                                   ),
                                   Expanded(
                                     child: Container(
-                                      color: Colors.black,
+                                      color: Colors.transparent,
+                                      child: TwoValueCard(
+                                        text: "Last APP Action",
+                                        txtclr: Colors.orange[900],
+                                        value: "2 Minutes Ago",
+                                        subwidget: null,
+                                        clr: null,
+                                      ),
                                     ),
                                   ),
                                 ],
@@ -88,7 +100,14 @@ class UserStatus extends StatelessWidget {
                             ),
                             Expanded(
                                 child: Container(
-                              color: Colors.yellow,
+                              color: Colors.transparent,
+                              child: TwoValueCard(
+                                        text: "User Status",
+                                        txtclr: Colors.orange[900],
+                                        value: "Driving",
+                                        subwidget: null,
+                                        clr: null,
+                                      ),
                             ))
                           ],
                         ),
@@ -100,7 +119,10 @@ class UserStatus extends StatelessWidget {
             ),
             Expanded(
               child: Container(
-                color: Colors.orange[900],
+width: double.infinity,
+decoration: BoxDecoration(
+  
+),
               ),
             ),
           ],
@@ -108,7 +130,4 @@ class UserStatus extends StatelessWidget {
       ),
     );
   }
-}
-
-
 }
---
+++ all_fixs/lib/Views/Widgets/Features/DeviceInfo/two_value_card.dart
@@ -0,0 +1,57 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TwoValueCard extends StatelessWidget {
+  final String text;
+  final String value;
+  final Widget? subwidget;
+  final Color? clr;
+  final Color? txtclr;
+
+  TwoValueCard({
+    required this.text,
+    required this.value,
+    required this.clr,
+    required this.subwidget,
+    required this.txtclr,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      width: double.infinity,
+      child: Card(
+        elevation: 10,
+        color: clr ?? Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.nunito(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            subwidget ??
+                Text(
+                  value,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.nunito(
+                    fontWeight: FontWeight.w900,
+                    color: txtclr ?? Colors.pink,
+                    fontSize: 18,
+                  ),
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
